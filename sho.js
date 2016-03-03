893c444b0c76f5a4cd82e80ad89b956c9a4b964b
csauve <sauvec@abebooks.com>
2016-03-02 21:40:08 -0800

    Add timer option

--- sho.js
+++ sho.js
@@ -13,21 +13,23 @@ var md = new Remarkable({
   }
 });
 
-module.exports = function(sourceFile, destFile) {
+module.exports = function(sourceFile, destFile, minutes) {
   var mdSource = fs.readFileSync(sourceFile, "UTF-8");
   var html = md.render(mdSource)
-  var jadeLocals = {slides: html.split("<hr>")};
+  var jadeLocals = {slides: html.split("<hr>"), minutes: minutes};
   var output = jade.renderFile(__dirname + "/template.jade", jadeLocals);
   fs.writeFileSync(destFile, output);
 };
 
 if (require.main === module) {
   var argv = require("minimist")(process.argv.slice(2));
-  if (argv.help) return console.log("Usage: sho [input.md] [output.html] [--watch]");
-  sourceFile = argv._[0] || "slides.md"
-  destFile = argv._[1] || sourceFile.split(".")[0] + ".html"
+  if (argv.help) return console.log("Usage: sho [input.md] [output.html] [--watch] [--minutes <minutes>]");
+  var sourceFile = argv._[0] || "slides.md";
+  var destFile = argv._[1] || sourceFile.split(".")[0] + ".html";
+  var minutes = argv.minutes === true ? undefined : parseInt(argv.minutes)
+  if (minutes) console.log("Including " + minutes + " minute timer in output");
   var render = function() {
-    module.exports(sourceFile, destFile);
+    module.exports(sourceFile, destFile, minutes);
     console.log("Generated " + destFile);
   };
   render();
